bc867d1ef77c56df9f94eaa7ce50824bd91ca6f8
KhangDS <gausseuler159357@gmail.com>
2026-01-13 18:28:23 +0700

    Chore: add data_structure lesson

---
+++ src/data_structures.R
@@ -0,0 +1,21 @@
+x <- numeric(length = 3)
+x
+
+y <- character(length = 3)
+y
+
+z <- vector(mode = "numeric", length = 3)
+z
+
+matrix_M <- matrix(data = c(20, 10, 34, 28.01, 82, 100.5, 6, 98, 29, 4829, 128, 28),
+                   nrow = 3, byrow = TRUE)
+matrix_M
+
+matrix_A <- rbind(c(1, 2, 3, 4), c(5, 6, 7, 8), c(9, 9.6, -0.5, 12))
+matrix_A
+
+
+n <- 3
+m <- 4
+dynamic_mat <- matrix(0,nrow=n, ncol=m)
+dynamic_mat
